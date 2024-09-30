f9fc7bcc0cb445f336ed0e242128ff5ad8318957
Raiduts <rivaldorakun1253@gmail.com>
2024-09-30 22:38:40 +0700

    gfdfka

---
+++ app/src/main/java/com/example/testin/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.testin
+
+import android.app.Application
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    var btn : Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.btnDisco)
+
+        btn?.setOnClickListener {
+            finish()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val i : Intent = Intent(this, MainActivity::class.java)
+        startActivity(i)
+    }
+
+    /*override fun onResume() {
+        super.onResume()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(2000)
+            finish()
+        }
+    }*/
+}
---
+++ app/src/main/java/com/example/testin/MainActivityAldo.kt
@@ -0,0 +1,23 @@
+package com.example.testin
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivityAldo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.gurunji_dashboarad)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+    }
+}
